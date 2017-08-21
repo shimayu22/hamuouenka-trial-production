200eef6a56f39bc43eeced74b69dfd8bf14c437f
Yutaka Shimada <shimayu22@gmail.com>
2017-08-21 11:53:06 +0900

    広告追加(コード未入力)

---
+++ hamuouenka/AdViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AdViewController.swift
+//  hamuouenka
+//
+//  Created by 島田裕 on 2017/08/16.
+//  Copyright © 2017年 shimayu. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class AdViewController: UIViewController {
+    
+    @IBOutlet weak var bannerView: GADBannerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
